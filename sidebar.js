171c7533f345255d6733c0ac0d93c94263a58e8e
Leon <leon.migliazzi@wexa.it>
2024-01-29 17:47:10 +0100

    sidebar's problem  fixed

--- sidebar.js
+++ sidebar.js
@@ -2,6 +2,7 @@ const sidebar = document.getElementById('sidebar');
 const settingsIcon = document.getElementById('settings');
 
 settingsIcon.addEventListener("click", function() {
+    sidebar.classList.toggle('hidden');
     sidebar.classList.toggle('translate-x-56');
     settingsIcon.classList.toggle('text-violet-950');
     settingsIcon.classList.toggle('text-violet-200');
