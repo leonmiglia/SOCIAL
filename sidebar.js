db78a08e0005a33758838cbdc241fdd8179ef034
Leon <leon.migliazzi@wexa.it>
2024-01-31 09:20:18 +0100

    sidebar's apparition better

--- sidebar.js
+++ sidebar.js
@@ -2,7 +2,6 @@ const sidebar = document.getElementById('sidebar');
 const settingsIcon = document.getElementById('settings');
 
 settingsIcon.addEventListener("click", function() {
-    sidebar.classList.toggle('hidden');
     sidebar.classList.toggle('translate-x-56');
     settingsIcon.classList.toggle('text-violet-950');
     settingsIcon.classList.toggle('text-violet-200');
