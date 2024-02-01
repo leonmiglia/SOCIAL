d87b690c634a532acdfec1b853a384589a2fb5d4
Leon <leon.migliazzi@wexa.it>
2024-02-01 10:35:25 +0100

    sidebar disappear, clicking outside of it

--- sidebar.js
+++ sidebar.js
@@ -7,3 +7,12 @@ settingsIcon.addEventListener("click", function() {
     settingsIcon.classList.toggle('text-violet-200');
     settingsIcon.classList.toggle('dark:text-indigo-950');
 });
+
+document.onclick = function(e){
+    if (!settingsIcon.contains(e.target) && !sidebar.contains(e.target) && !sidebar.classList.contains('translate-x-56') ) {
+        settingsIcon.classList.toggle('text-violet-950');
+        settingsIcon.classList.toggle('text-violet-200');
+        settingsIcon.classList.toggle('dark:text-indigo-950');
+        sidebar.classList.add("translate-x-56");
+    }
+}
